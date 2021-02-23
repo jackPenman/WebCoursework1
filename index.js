e3c5cb88c3c99ce7ecbafb959254df94f7a7ff7b
drjock <drjock123@gmail.com>
2021-02-23 22:08:12 +0000

    adding hompage stub

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 const public = path.join(__dirname, 'public');
 
+var Datastore = require('nedb');
+db = new Datastore();
 
 const app = express();
 app.use(express.static(public));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.');
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(public, 'index.html'));
 })
